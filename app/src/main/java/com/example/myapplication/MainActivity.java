431fb0ff10902d9e008b554389c6387a95daf7eb
evan <evan@example.com>
2023-02-22 17:55:58 +0800

    添加反射打印所有方法值的代码,暂未解决,测试中

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -20,6 +22,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         main();
+    }
+
+    /**
+     * 打印某个类所有的字段
+     * @param obj
+     */
+    public static void printAllFields(Object obj){
+        Class    cls=obj.getClass();
+        Field[] fields=cls.getDeclaredFields();
+        System.out.println("共有"+fields.length+"个属性");
+        for (Field field:fields) {
+            field.setAccessible(true);
+            try {
+                System.out.println(field.getName()+":"+field.get(obj));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //遍历类中所有字段信息 https://blog.csdn.net/weixin_34346099/article/details/94036015
+    public static void reflect(Object o){
+        //获取参数类
+        Class cls = o.getClass();
+        //将参数类转换为对应属性数量的Field类型数组（即该类有多少个属性字段 N 转换后的数组长度即为 N）
+        Field[] fields = cls.getDeclaredFields();
+        for(int i = 0;i < fields.length; i ++){
+            Field f = fields[i];
+            f.setAccessible(true);
+            try {
+                //f.getName()得到对应字段的属性名，f.get(o)得到对应字段属性值,f.getGenericType()得到对应字段的类型
+//                System.out.println("属性名："+f.getName()+"；属性值："+f.get(o)+";字段类型：" + f.getGenericType());
+                Log.i(TAG,"属性名："+f.getName()+"；属性值："+f.get(o)+";字段类型：" + f.getGenericType());
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Log.i(TAG,"ReflectUtil error:"+e.toString());
+            }
+        }
     }
 
     private void main() {
@@ -37,7 +78,32 @@ public class MainActivity extends AppCompatActivity {
 //            Log.i(TAG,"222222222222222");
             WifiManager wifiManager = (WifiManager) this.getSystemService(WIFI_SERVICE);
             WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-            Log.i(TAG,"name ------> " + wifiInfo.getSSID());
+//            Log.i(TAG,"name ------> " + wifiInfo.get);
+//            reflect(wifiInfo);
+//遍历所有方法
+            Class<?> wifiInfoClass = Class.forName(android.net.wifi.WifiInfo.class.getName());
+            Method[] methods = wifiInfoClass.getDeclaredMethods();
+            Log.i(TAG,"total function is " + methods.length);
+            for (Method method : methods) {
+                Log.i(TAG," " + method.getName());
+
+            }
+            ////////////////
+            Object o = android.net.wifi.WifiInfo.class;
+            Class cls = o.getClass();
+            //将参数类转换为对应属性数量的Field类型数组（即该类有多少个属性字段 N 转换后的数组长度即为 N）
+            Field[] fields = cls.getDeclaredFields();
+            for(int i = 0;i < fields.length; i ++){
+                Field f = fields[i];
+                f.setAccessible(true);
+                try {
+                    Log.i(TAG,"属性名："+f.getName()+"；属性值："+f.get(o)+";字段类型：" + f.getGenericType());
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    Log.i(TAG,"ReflectUtil error:"+e.toString());
+                }
+            }
 
 //            end
             // Get the IP address of the WiFi interface
