9fe9549b49f964f1fe68faf1b56de101b43dbcf3
evan <evan@123.com>
2023-02-26 21:57:53 +0800

    增加viewbinding支持,添加控件并设置默认焦点

--- app/src/main/java/com/example/myapplication/greendao/UIActivity1.java
+++ app/src/main/java/com/example/myapplication/greendao/UIActivity1.java
@@ -2,21 +2,33 @@ package com.example.myapplication.greendao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityUiactivity1Binding;
 
 public class UIActivity1 extends AppCompatActivity {
     private static final String TAG = "UIActivity1:evan";
+    private ActivityUiactivity1Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_uiactivity1);
+        binding = ActivityUiactivity1Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         if(Utils.firstRun(this)){
             Log.i(TAG,"first in app");
         }else{
             Log.i(TAG,"not first in app");
         };
+        binding.uiActivity1BtnJump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UIActivity1.this,UIActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/greendao/UIActivity2.java
+++ app/src/main/java/com/example/myapplication/greendao/UIActivity2.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UIActivity2 extends AppCompatActivity {
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityUiactivity2Binding;
 
+public class UIActivity2 extends AppCompatActivity {
+    ActivityUiactivity2Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_uiactivity2);
+        binding = ActivityUiactivity2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initData();
+    }
+
+    private void initData() {
+        binding.uiEtName.requestFocus();
     }
 }
