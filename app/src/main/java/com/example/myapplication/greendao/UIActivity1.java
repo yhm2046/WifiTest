02a1b936bdde1c04282eb6b036868abf6d1d0f78
evan <evan@123.com>
2023-02-26 21:15:38 +0800

    添加判断是否首次进入app的方法

--- app/src/main/java/com/example/myapplication/greendao/UIActivity1.java
+++ app/src/main/java/com/example/myapplication/greendao/UIActivity1.java
@@ -3,12 +3,20 @@ package com.example.myapplication.greendao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.myapplication.R;
 
 public class UIActivity1 extends AppCompatActivity {
-
+    private static final String TAG = "UIActivity1:evan";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uiactivity1);
+        if(Utils.firstRun(this)){
+            Log.i(TAG,"first in app");
+        }else{
+            Log.i(TAG,"not first in app");
+        };
     }
 }
---
+++ app/src/main/java/com/example/myapplication/greendao/Utils.java
@@ -0,0 +1,63 @@
+package com.example.myapplication.greendao;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.myapplication.BuildConfig;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * tools class,2023.2.26 Sunday
+ */
+public class Utils {
+    /**
+     * judge whether is first run app
+     * @param context   context
+     * @return boolean
+     */
+    public static boolean firstRun(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("FirstRun",0);
+        boolean first_run = sharedPreferences.getBoolean("First",true);
+        if (first_run){
+            sharedPreferences.edit().putBoolean("First",false).apply();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
