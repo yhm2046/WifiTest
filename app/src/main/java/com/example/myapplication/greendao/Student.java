d7af921146adc318cb08f6642847d51c94e1cfa5
evan <evan@123.com>
2023-02-26 20:50:08 +0800

    添加student类,会自动生成实体类的dao文件

---
+++ app/src/main/java/com/example/myapplication/greendao/Student.java
@@ -0,0 +1,2 @@
+package com.example.myapplication.greendao;public class Student {
+}
---
+++ app/src/main/java/com/example/myapplication/greendao/UIActivity1.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.greendao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UIActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uiactivity1);
+    }
+}
